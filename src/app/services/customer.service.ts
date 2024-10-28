c9115ba4b2ca1ec11b8f0a16d5b3452c507937d0
tchoumi313 <75470956+tchoumi313@users.noreply.github.com>
2024-10-28 22:41:26 +0100

    Done, it's clean

--- src/app/services/customer.service.ts
+++ src/app/services/customer.service.ts
@@ -1,7 +1,7 @@
+/*import { HttpClient } from "@angular/common/http";
 import { Injectable } from '@angular/core';
-import {HttpClient} from "@angular/common/http";
-import {Observable} from "rxjs";
-import {Customer} from "../model/customer.model";
+import { Observable } from "rxjs";
+import { Customer } from "../model/customer.model";
 
 @Injectable({
   providedIn: 'root'
@@ -12,27 +12,74 @@ export class CustomerService {
 
   constructor(private http: HttpClient) { }
 
-  public getCustomers():Observable<Array<Customer>>{
-    return this.http.get<Array<Customer>>(this.host+'/customers');
+  public getCustomers(): Observable<Array<Customer>> {
+    return this.http.get<Array<Customer>>(this.host + '/customers');
   }
 
   public getCustomer(id: number): Observable<Customer> {
     return this.http.get<Customer>(`${this.host}/customers/${id}`);
   }
 
-  public searchCustomers(name: string):Observable<Array<Customer>>{
-    return this.http.get<Array<Customer>>(this.host+`/customers/search?keyword=${name}`);
+  public searchCustomers(name: string): Observable<Array<Customer>> {
+    return this.http.get<Array<Customer>>(this.host + `/customers/search?keyword=${name}`);
   }
 
-  public saveCustomer(customer: Customer):Observable<Customer>{
-    return this.http.post<Customer>(this.host+'/customers', customer);
+  public saveCustomer(customer: Customer): Observable<Customer> {
+    return this.http.post<Customer>(this.host + '/customers', customer);
   }
 
-  public deleteCustomer(id: number){
-    return this.http.delete(this.host+"/customers/"+id);
+  public deleteCustomer(id: number) {
+    return this.http.delete(this.host + "/customers/" + id);
   }
 
-  public updateCustomer(customer: Customer):Observable<Customer>{
-    return this.http.put<Customer>(this.host+`/customers/${customer.id}`, customer);
+  public updateCustomer(customer: Customer): Observable<Customer> {
+    return this.http.put<Customer>(this.host + `/customers/${customer.id}`, customer);
+  }
+}
+*/
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from '@angular/core';
+import { Observable } from "rxjs";
+import { Customer } from "../model/customer.model";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CustomerService {
+
+  // Base URL for the JSON server
+  host: string = 'http://localhost:8000';
+
+  constructor(private http: HttpClient) { }
+
+  // Fetch all customers from the JSON server
+  public getCustomers(): Observable<Array<Customer>> {
+    return this.http.get<Array<Customer>>(`${this.host}/customers`);
+  }
+
+  // Fetch a single customer by ID
+  public getCustomer(id: number): Observable<Customer> {
+    return this.http.get<Customer>(`${this.host}/customers/${id}`);
+  }
+
+  // Search customers by name using a query parameter
+  public searchCustomers(name: string): Observable<Array<Customer>> {
+    // JSON server supports filtering with query parameters
+    return this.http.get<Array<Customer>>(`${this.host}/customers?name_like=${name}`);
+  }
+
+  // Save a new customer to the JSON server
+  public saveCustomer(customer: Customer): Observable<Customer> {
+    return this.http.post<Customer>(`${this.host}/customers`, customer);
+  }
+
+  // Delete a customer by ID
+  public deleteCustomer(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.host}/customers/${id}`);
+  }
+
+  // Update an existing customer
+  public updateCustomer(customer: Customer): Observable<Customer> {
+    return this.http.put<Customer>(`${this.host}/customers/${customer.id}`, customer);
   }
 }
